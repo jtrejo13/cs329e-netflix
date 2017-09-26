1ef6c724bf7c1d8ee500fdd5ade8394c0018aba7
Juan <jtrj33@gmail.com>
2017-09-26 11:20:06 -0500

    Comment out print statements in Netflix.py. Update Netflix.html

--- Netflix.py
+++ Netflix.py
@@ -23,15 +23,15 @@ def create_cache(filename):
     filePath = '/u/fares/public_html/netflix-caches/' + filename
 
     if path.isfile(filename):                   #if file in my local computer
-        print('cache found locally')
+        # print('cache found locally')
         with open(filename, "rb") as f:
             cache = pickle.load(f)
     elif path.isfile(filePath):                 #else if file in cs computer
-        print('cache found in cs computers')
+        # print('cache found in cs computers')
         with open(filePath, "rb") as f:
             cache = pickle.load(f)
     else:                                       #else retreive from web
-        print('cache retreived from web')
+        # print('cache retreived from web')
         webAddress = 'http://www.cs.utexas.edu/users/fares/netflix-caches/' + \
             filename
         bytes = get(webAddress).content
@@ -43,16 +43,16 @@ def create_cache(filename):
 AVERAGE_RATING = 3.60428996442
 YEAR_BY_MOVIE_ID = create_cache(
     'JT26983-MovieYearByMovieID.pickle')
-print('success YEAR_BY_MOVIE_ID')
+# print('success YEAR_BY_MOVIE_ID')
 AVERAGE_MOVIE_RATING_BY_RELEASE_YEAR = create_cache(
     'JT26983-AvgMovieRatingsByReleaseYear.pickle')
-print('success AVERAGE_MOVIE_RATING_BY_RELEASE_YEAR')
+# print('success AVERAGE_MOVIE_RATING_BY_RELEASE_YEAR')
 AVERAGE_MOVIE_RATING_BY_MOVIE_ID = create_cache(
     'JT26983-AvgMovieRatingByMovieID.pickle')
-print('success AVERAGE_MOVIE_RATING_BY_MOVIE_ID')
+# print('success AVERAGE_MOVIE_RATING_BY_MOVIE_ID')
 AVERAGE_RATING_BY_CUST_ID_AND_REL_YEAR = create_cache(
     'JT26983-AvgRatingByCustomerIDAndReleaseYear.pickle')
-print('success AVERAGE_RATING_BY_CUST_ID_AND_REL_YEAR')
+# print('success AVERAGE_RATING_BY_CUST_ID_AND_REL_YEAR')
 ACTUAL_CUSTOMER_RATING = create_cache(
     "JT26983-ActualRatingByCustomerIDAndMovieID.pickle")
 
